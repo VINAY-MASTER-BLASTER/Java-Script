bb7a78644cc636a2665013b584278e2ae133bbfd
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-02-12 22:29:52 +0530

    first commit

---
+++ Arrays/01_Arrays.js
@@ -0,0 +1,56 @@
+// what is Array - Arrays is a datastrcture 
+
+// let a = [38,"Hello",true,undefined,null]
+
+
+// let a = []
+// a = [1,2,3,4,5,6,7]
+// console.log(a)
+
+
+
+// constrcutor function
+// let arr = new Array(0,9,8,7,6,5)
+// console.log(arr)
+
+
+// index 
+// let a = [1,2,3,4,5,6,7]
+// console.log(a[4])
+
+
+// Array itration 
+// for(let i = 0; i<7; i++){
+//     console.log(a[i])
+// }
+
+// Print thr Reviser Array 
+// let arr = [5,4,3,3,2,1]
+// for(let i = 5; i>=0; i--){
+//     console.log(arr[i])
+// }
+
+// add the Array index 
+// let arr = [1,2,3,4,5]
+// add the Array
+// arr[5] = 45
+
+// replace 
+// arr[1] = "two"
+
+// arr[7] = "Hello"
+// console.log(arr)
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i = 0; i<9; i=i+2){
+//     console.log(arr[i])
+// }
+
+
+let even = [23,32,34,3,43,434,55,54,52,545]
+for(let i = 0; i<10; i++){
+    if(even[i]%2==0){
+        console.log(even[i])
+    }
+}
