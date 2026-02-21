69d02a14ac9af68d8c0489b7cdbcc4309041cf65
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-02-22 01:24:59 +0530

    Destructuring.js

---
+++ Object/Destructuring/Destructuring.js
@@ -0,0 +1,21 @@
+//? destructuring 
+
+//? Array
+// let arr  = [1,2,3]
+// let [a,b,c] = arr
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// ? Objects 
+// let obj = {
+//     name : "vinay",
+//     age : 21,
+//     occupations : "Developer",
+//     years : 3
+// }
+// let {name,age,occupations,years} = obj
+// console.log(name)
+// console.log(age)
+// console.log(occupations)
+// console.log(years)
